9c8352e7495fd9949f940d579f50bce6a0202152
Julia Lisec <julialisec12@gmail.com>
2020-11-20 05:50:02 +0100

    reconstructed array

--- app.js
+++ app.js
@@ -18,12 +18,15 @@ class Products {
         try {
             let result = await fetch('products.json');
             let data = await result.json();
+
             let products = data.items;
             products = products.map (item => {
                 const {title, price} = item.fields;
                 const {id} = item.sys;
-                const image = item.field.image.fields.file.url;
+                const image = item.fields.image.fields.file.url;
+                return {title, price, id, image}
             })
+            return products;
         } catch (error) {
             console.log(error);
         }
